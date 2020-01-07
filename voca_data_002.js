93c4d83ddd841c89b1a6a62d6409cd8e1b603e8a
fantasia <fantasiacjh@gmail.com>
2020-01-07 22:53:56 +0900

    Update voca 002

---
+++ voca_data_002.js
@@ -0,0 +1,16 @@
+// 영어, 한국어, 발음기호, audio url key
+var vocaDatas = 
+[
+["hold-held", "들다"],
+["lovely", "사랑스러운"],
+["closely", "가까이"],
+["worry-worried", "걱정하다"],
+["look for-looked for", "~를 찾다"],
+["cupboard", "찬장"],
+["tie up-tied up", "묶어 놓다"],
+["untie-untied", "풀다"],
+["afraid", "무서워하는"],
+["hit-hit", "때리다"],
+["run out of-ran out of", "~로부터 도망 나오다"],
+["learn-learned", "배우다"]    
+]
